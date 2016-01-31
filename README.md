1d67a7cd075eab96dfc2f712a7555e8a70102cec
huw <huw1999@gmail.com>
2016-01-31 18:17:58 +1100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# GitHub to Discourse
+This is very simple.
+
+It will forward any 'push' webhooks it receives to a specified thread on a [Discourse](https://discourse.org) forum. I couldn't find anything similar around, presumably because people don't really use Discourse to publish new commits. But hey, maybe there's a use-case somewhere.
+
+# Install
+1. `npm install -g git+ssh://git@github.com/huw/github-to-discourse.git`
+2. `mv config.json.0 config.json` and edit properties (see below)
+3. `g2d`
+
+# Config.json Format
+You can rename `config.json.0` or just copy this:
+```
+    {
+        "discourse": {
+            "api_key": "",          // Your discourse API Key (Ask an admin for one)
+            "api_user": "",         // Your discourse username
+            "domain": "",           // Domain of your discourse server (format: "meta.discourse.org")
+            "https_enabled": false, // Use https for the discourse server, optional (default: false)
+            "topic_id": 0           // Topic ID to post comments to (See number at end of URL, eg. `meta.discourse.org/t/topic/233`)
+        },
+        "onebox_enabled": false,    // If your forum converts a commit link to a fancy box and you'd rather use that, set to true. Optional (default: false)
+        "webhook": {
+            "path": "",             // Path name that GitHub will send webhooks to (format: "path-name", omit slashes)
+            "port": 8080,           // Port to run on, optional (default: 8080)
+            "secret": ""            // GitHub webhook secret
+        }
+    }
+```
+
+Edit the propeties as appropriate. Everything except `discourse.https_enabled`, `onebox_enabled` and `webhook.port` are mandatory.
---
+++ g2d.js
@@ -0,0 +1,118 @@
+#! /usr/bin/env node
+var fs      = require('fs');
+var http    = require('http')
+var request = require('request')
+
+var config = JSON.parse(
+    fs.readFileSync('config.json')
+);
+
+// Ensure all files in object
+var mandatory_discourse_props = ["api_key", "api_user", "domain", "topic_id"]
+var mandatory_webhook_props = ["secret"]
+
+for (property of mandatory_discourse_props) {
+    if (!config.discourse.hasOwnProperty(property)) {
+        throw new Error("Required property discourse." + 
+            property + 
+            " not found in config.json")
+    }
+}
+
+for (property of mandatory_webhook_props) {
+    if (!config.webhook.hasOwnProperty(property)) {
+        throw new Error("Required property webhook." + 
+            property + 
+            " not found in config.json")
+    }
+}
+
+var handler = require('github-webhook-handler')({
+    path: config.webhook.path,
+    secret: config.webhook.secret
+})
+
+// Start the server with our handler
+var port = config.webhook.hasOwnProperty('port') ? config.webhook.port : 8080
+http.createServer(function (req, res) {
+    handler(req, res, function (err) {
+        res.statusCode = 404
+        res.end('Not Found')
+    })
+}).listen(port, function() {
+    console.log("Accepting webhooks to /" + config.webhook.path + " on port " 
+                + port)
+})
+
+handler.on('push', function(event) {
+
+    console.log("Received push from " + event.respository.full_name)
+    var commits = event.payload.commits
+
+    // Handle multiple commits in one webhook
+    for (var commit of commits) {
+
+        // BUILD THE MESSAGE
+        // We put together the forum post in Markdown
+        // by splitting the commit message and adding
+        // quote delimeters (`>`) to the beginning of
+        // each. If it's the first line, add a `#` to
+        // highlight it as a title. If your forum has
+        // oneboxes and you'd rather use them, we can
+        // skip this.
+
+        var comment_text = ""
+        if (config.hasOwnProperty('onebox_enabled') && config.onebox_enabled) {
+            commit = commit.message.split("\n")
+            comment_text = "_latest changes on github:_\n\n---\n"
+
+            for (var x = 0; x < commit.length; x++) {
+                var line = commit[x]
+                comment_text += "> " + (x==0 ? "# " : "") + line + "\n"
+            }
+
+            comment_text += 
+                "\n\n---\n_[more details at github](" +
+                commit.url + 
+                ") - this is an automated post_"
+        } else {
+            comment_text = commit.url
+        }
+
+        // MAKE THE COMMENT
+        // Given the details you added to config.json
+        // we make a POST to your thread with details
+        // about your commit.
+
+        request({
+            url: 
+                (
+                    (
+                        config.discourse.hasOwnProperty('https_enabled') && 
+                        config.discourse.https_enabled
+                    ) 
+                    ? "https://" 
+                    : "http://"
+                ) +
+                config.discourse.domain +
+                "/posts",
+            method: "POST",
+            qs: {
+                'api_key': config.discourse.api_key,
+                'api_username': config.discourse.api_user
+            },
+            form: {
+                topic_id: config.discourse.topic_id,
+                raw: comment_text
+            }
+        }, function(err, res, body) {
+            if (err) throw err
+            console.log("Success: " + commit.id)
+            console.log(res)
+        })
+    };
+})
+
+handler.on('ping', function(event) {
+    console.log("Received ping: " + event.payload.zen)
+})
